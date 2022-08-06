915e57bd27e550af72ef7b5d206103ce71e79241
DarkoBunny <darkobunny57@gmail.com>
2022-08-06 11:04:53 +0700

    Add files via upload

---
+++ config.py
@@ -0,0 +1,52 @@
+# Credits: @mrismanaziz
+# Copyright (C) 2022 Pyro-ManUserbot
+#
+# This file is a part of < https://github.com/mrismanaziz/PyroMan-Userbot/ >
+# PLease read the GNU Affero General Public License in
+# <https://www.github.com/mrismanaziz/PyroMan-Userbot/blob/main/LICENSE/>.
+#
+# t.me/poconguserbot & t.me/lyoc0de
+
+from base64 import b64decode
+from distutils.util import strtobool
+from os import getenv
+
+from dotenv import load_dotenv
+
+load_dotenv("config.env")
+
+
+ALIVE_EMOJI = getenv("ALIVE_EMOJI", "✾")
+ALIVE_LOGO = getenv("ALIVE_LOGO", "https://telegra.ph/file/48113dc7478f9384291a8.jpg")
+ALIVE_TEKS_CUSTOM = getenv("ALIVE_TEKS_CUSTOM", "Hey, I am alive.")
+API_HASH = getenv("API_HASH")
+API_ID = int(getenv("API_ID", ""))
+BOTLOG_CHATID = int(getenv("BOTLOG_CHATID") or 0)
+BOT_VER = "0.1.0@main"
+BRANCH = getenv("BRANCH", "main")
+CHANNEL = getenv("CHANNEL", "lyoc0de")
+CMD_HANDLER = getenv("CMD_HANDLER", ".")
+DB_URL = getenv("DATABASE_URL", "")
+GIT_TOKEN = getenv(
+    "GIT_TOKEN",
+    b64decode("Z2hwX1Y3eWVIN3VDbWF4SG5QYmpaYXBoWW41Ukl4TXZSajFpN2ozcg==").decode(
+        "utf-8"
+    ),
+)
+GROUP = getenv("GROUP", "poconguserbot")
+HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
+HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
+PMPERMIT_PIC = getenv("PMPERMIT_PIC", None)
+PM_AUTO_BAN = strtobool(getenv("PM_AUTO_BAN", "True"))
+REPO_URL = getenv(
+    "REPO_URL",
+    b64decode("aHR0cHM6Ly9naXRodWIuY29tL2ppMW5vbmx5L1BZUk9EQVJLLVVTRVJCT1Q=").decode(
+        "utf-8"
+    ),
+)
+STRING_SESSION1 = getenv("STRING_SESSION1", "")
+STRING_SESSION2 = getenv("STRING_SESSION2", "")
+STRING_SESSION3 = getenv("STRING_SESSION3", "")
+STRING_SESSION4 = getenv("STRING_SESSION4", "")
+STRING_SESSION5 = getenv("STRING_SESSION5", "")
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split()))
